e9e6ac9750f58decf04f8b6943d3d272afa22b1d
Sam Corkery <134240832+Piemaster37@users.noreply.github.com>
2023-12-11 20:00:49 -0600

    login page start

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+export default function Login() {
+    return (
+        <div class ="login">
+        <h1>Water-you Doing</h1>
+        <h4>UserName</h4>
+        <input type = "text"></input>
+        <h4>Password</h4>
+        <input type="password"></input>
+        </div>
+    )
+}
