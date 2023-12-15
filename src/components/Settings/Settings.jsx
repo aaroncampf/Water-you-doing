eea83ae5d513ec91d543922b888b692f11aa9c52
oprah-cares <emilykleinstl@gmail.com>
2023-12-15 15:18:53 -0600

    Created dropdown for color picker

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,8 +1,28 @@
 // Settings.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import ColorThemeDropdown from '../DropDown/DropDown'; 
 
 const Settings = () => {
-  return <div>Settings Page</div>;
+  const [selectedTheme, setSelectedTheme] = useState('default');
+
+  const themes = [
+    { value: 'default', label: 'Default Theme' },
+    { value: 'dark', label: 'Dark Theme' },
+    { value: 'light', label: 'Light Theme' },
+  ];
+
+  const handleThemeChange = (event) => {
+    setSelectedTheme(event.target.value);
+    // You can add logic here to apply the selected theme to your app
+  };
+
+  return (
+    <div>
+      <h2>Settings Page</h2>
+      <p>Select App Theme:</p>
+      <ColorThemeDropdown themes={themes} selectedTheme={selectedTheme} onChange={handleThemeChange} />
+    </div>
+  );
 };
 
 export default Settings;
