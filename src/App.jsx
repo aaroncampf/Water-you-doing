42a802575d994fdfb7dfaf18dd8f8a6c84a15ad7
anitha-gk <139910108+anitha-gk@users.noreply.github.com>
2023-12-16 17:59:06 -0600

    Updated the file

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import Analytics from './components/Analytics/Analytics';
+import Settings from './components/Settings/Settings';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
