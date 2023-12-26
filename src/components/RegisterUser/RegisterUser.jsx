20ab930e370bda4dbf1df5a83f68ca3e58f8e929
anitha-gk <139910108+anitha-gk@users.noreply.github.com>
2023-12-26 15:42:56 -0600

    Added register user page updates

--- src/components/RegisterUser/RegisterUser.jsx
+++ src/components/RegisterUser/RegisterUser.jsx
@@ -1,59 +1,115 @@
 // RegisterUser.jsx
 import React from 'react';
+import { useState } from "react";
 
-const RegisterUser = () => {
-  return <div>
-    Join the community
-    of over XXXX water drinkers
-    <tr>
-      <td>
-        <h1>First Name</h1> 
-      </td>
-      <td>
-        <h1>Last Name</h1> 
-      </td>
-    </tr>
-    <tr>
-      <td>
-      <input id="firstname" type="text"/> 
-      </td>
-      <td>
-      <input id="lastname" type="text"/> 
-      </td>
-    </tr>
-    <tr>
-      <td colSpan={2}>
-        <h1>Email</h1>
-      </td>
-    </tr>
-    <tr>
-      <td colSpan={2}>
-        <input id="email" type="email"/>
-      </td>
-    </tr>
-    
-    <tr>
-      <td colSpan={2}>
-        <h1>UserName</h1>
-      </td>
-    </tr>
-    <tr>
-      <td colSpan={2}>
-        <input id="username" type="text"/>
-      </td>
-    </tr>
-    <tr>
-      <td colSpan={2}>
-        <h1>Password</h1>
-      </td>
-    </tr>
-    <tr>
-      <td colSpan={2}>
-        <input id="password" type="password"/>
-      </td>
-    </tr>
+import './RegisterUser.css'
 
-    </div>;
-};
+
+function RegisterUser({events}) {
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [userID, setUserID] = useState();
+  const [password, setPassword] = useState();
+
+  const {handleUserLogin, handleNewUser} = events;
+
+
+  function clickRegisterUser(e){
+    const payload = {
+      fname: firstName,
+      lname: lastName,
+      email: email,
+      userID: userID,
+      password: password
+  };
+  
+  fetch('http://localhost:8080/RegisterUser', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+  }).then(response => response.json())
+  .then(jsondata => {
+    console.log(jsondata);
+    const resultJSON = JSON.parse(JSON.stringify(jsondata));
+    console.log(resultJSON.STATUS);
+    alert("User Registered Successfully");
+    if (resultJSON.STATUS == "Success") {
+      handleUserLogin(userID);
+      handleNewUser(false);   
+    }
+   }
+  ).catch(error => {
+    alert("Error submitting request");
+       
+  });   
+}
+
+
+  return (
+    <form className="RegisterUser">
+        <div className="RegisterUserDiv">
+                
+                  <table border='1'>
+                  <tr>
+                    <td>
+                      First Name 
+                    </td>
+                    <td>
+                      Last Name 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                    <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} 
+                      type="text"/> 
+                    </td>
+                    <td>
+                    <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} type="text"/> 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Email</td>
+                    <td>
+                    <input placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} type="email"/> 
+                    </td>
+                  </tr>
+                  
+                  <tr>
+                    <td>User ID</td>
+                    <td>
+                    <input placeholder='Enter User Name' value={userID} onChange={(e) => setUserID(e.target.value)} type="text"/> 
+                    </td>
+                  </tr>
+                  <tr>
+                  <td>
+                      Password
+                    </td>
+                    <td>
+                    <input placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} type="password"/> 
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2}>
+                    <input type="button" value="Register User" onClick={ e => clickRegisterUser(e)} ></input>
+                    </td>
+
+                  </tr>
+                  
+                  </table>
+                  
+                  
+            </div>
+     </form> 
+
+  )
+  
+}
 
 export default RegisterUser;
+
+
+
