e374d18776515e553f3fe950cf4ed1bf1d44c7ef
oprah-cares <emilykleinstl@gmail.com>
2024-01-04 18:06:24 -0600

    updated app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import Analytics from './components/Analytics/Analytics';
 import Settings from './components/Settings/Settings';
+import { ReminderProvider } from './components/ReminderContext';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <NavBar />
-        <Routes>
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <ReminderProvider>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </ReminderProvider>
     </Router>
   );
 };
 
 export default App;
-
-
