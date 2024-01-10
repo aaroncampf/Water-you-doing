2778c7de4938041ef7d76a13ea2ced112b8c88bd
oprah-cares <emilykleinstl@gmail.com>
2024-01-10 11:58:18 -0600

    making home page

--- backend/wateryoudoingbackend/api/urls.py
+++ backend/wateryoudoingbackend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, update_water_consumption
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
+    path('update_water_consumption/', update_water_consumption, name='update_water_consumption'),
 ]
--- backend/wateryoudoingbackend/api/views.py
+++ backend/wateryoudoingbackend/api/views.py
@@ -1,8 +1,14 @@
+from datetime import date
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+import json
+from .models import WaterConsumption
 
 
 class UserRegistrationView(generics.CreateAPIView):
@@ -19,8 +25,27 @@ class UserRegistrationView(generics.CreateAPIView):
 
 class UserLoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         return Response({'user_id': user.id})
+
+@require_POST
+@login_required
+def update_water_consumption(request):
+    data = json.loads(request.body)
+    amount = data.get('amount')
+    user = request.user
+    today = date.today()
+    
+    water_consumption, created = WaterConsumption.objects.get_or_create(
+        user=user,
+        date=today,
+        defaults={'amount': amount}
+    )
+
+    if not created:
+        water_consumption.amount = amount
+        water_consumption.save()
+
+    return JsonResponse({'message': 'Water consumption updated successfully'})
